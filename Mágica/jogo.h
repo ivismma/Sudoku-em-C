af6786c0b4cf4de3b549f672197d153850cc0ecb
Ivis Muzi <118779139+ivismma@users.noreply.github.com>
2024-06-10 22:37:07 -0300

    Update jogo.h

--- Mágica/jogo.h
+++ Mágica/jogo.h
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 #include <windows.h>
 #include <stdbool.h>
 #include <ncurses.h>
@@ -11,22 +12,25 @@ typedef struct{
 } tPos;
 
 // inputoutput.c
-tPos selecionar(short *tentativa, bool M2[9][9]);
-void input(tPos *escolha, int tecla, bool *selecionado, bool M2[9][9]);
-void exibirSetas(tPos escolha);
-void apagarSetas(tPos escolha);
-void apagarSelecao(tPos atual);
+tPos selecionar(short *tentativa, short M1[9][9]);
+void input(tPos *escolha, int tecla, bool *selecionado, short M1[9][9]);
+bool verificaAcerto(short tentativa, tPos pos, short M1[9][9]);
 tPos converterPos(tPos pos);
 short palpitar();
 
 // visual.c
+void desenhaJogo(short M1[9][9]);
 void desenhaArea();
-void exibeNumeros(short M[9][9], bool M2[9][9]);
+void exibeNumeros(short M[9][9]);
+void exibeAcerto(tPos pos, short num);
 void areaEscolha();
 void mostrarEscolha(char escolhaAtual);
+void exibirSetas(tPos escolha);
+void apagarSetas(tPos escolha);
+void apagarSelecao(tPos atual);
 void mostrarSelecao();
 void mostrarInfo();
 void atualizarStats(short enc, short erros);
 
 // filereading.c
-bool lerMapa(short M1[9][9], bool M2[9][9], short *encontrados);
+bool lerMapa(short M1[9][9], short *encontrados);
